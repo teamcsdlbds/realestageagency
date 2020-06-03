70c1de01981a53003c2011aa2ec9c5f4e9155f37
ductoinguyen <55026343+ductoinguyen@users.noreply.github.com>
2020-06-03 23:44:54 +0700

    edit title BĐS quan tâm

--- app/favorite-real-estate.php
+++ app/favorite-real-estate.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quản lý BĐS</title>
+    <title>BĐS quan tâm</title>
     <link rel="stylesheet" href="../css/my-details.css">
     <link rel="stylesheet" href="../css/common.css">
     <link rel="stylesheet" href="../css/grid.css">
